3d639def04df1f1a6227ea9d03dd28bbdf796ef3
CTC-Chevelle <115500466+CTC-Chevelle@users.noreply.github.com>
2025-10-22 22:49:17 -0400

    Create request_controller.py

---
+++ App/controllers/request_controller.py
@@ -0,0 +1,15 @@
+from App.models import Request
+from App.database import db
+
+def get_all_requests():
+  return Request.query.all()
+
+def get_requests_by_drive(drive_id):
+  return Request.query.filter_by(driveId = drive_id).all()
+
+def update_request_status(request_id, status):
+  request = Request.query.get(request_id)
+  if request:
+    request.status = status
+    db.session.commit()
+  return request
