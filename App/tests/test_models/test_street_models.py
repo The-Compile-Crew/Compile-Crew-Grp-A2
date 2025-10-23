37dfded108981507e563afd295dba2edd8056581
AishaLewis3 <aisha.lewis3@my.uwi.edu>
2025-10-23 19:38:08 -0400

    Add unit tests for Street model

---
+++ App/tests/test_models/test_street_models.py
@@ -0,0 +1,13 @@
+import unittest
+from App.models import Street
+
+class StreetModelUnitTests(unittest.TestCase):
+
+    def test_create_street(self):
+        street = Street("Main Street")
+        self.assertEqual(street.name, "Main Street")
+
+    def test_street_json(self):
+        street = Street("Main Street")
+        expected = {'streetId': None, 'name': 'Main Street'}
+        self.assertDictEqual(street.toJSON(), expected)
