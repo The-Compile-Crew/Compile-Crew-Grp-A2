a3f0f43b5a8e2f1726c31f14e04c45fcba8d688c
AishaLewis3 <aisha.lewis3@my.uwi.edu>
2025-10-24 08:36:08 -0400

    Add resident controller with request and drive access functions

--- App/controllers/resident_controller.py
+++ App/controllers/resident_controller.py
@@ -1,26 +1,26 @@
-from App.models import Resident, Driver, Request
+from App.models import Resident, Driver, Request, Drive
 from App.database import db
 
 def create_resident(name, street_id):
-  resident = Resident(name, street_id)
-  db.session.add(resident)
-  db.session.commit()
-  return resident 
+    resident = Resident(name, street_id)
+    db.session.add(resident)
+    db.session.commit()
+    return resident
 
 def get_all_residents():
-  return Resident.query.all()
+    return Resident.query.all()
 
 def get_resident(resident_id):
-  return Resident.query.get(resident_id)
+    return Resident.query.get(resident_id)
 
 def get_drives_by_street(street_id):
-  return Drive.query.filter_by(streetId = street_id).all()
+    return Drive.query.filter_by(streetId=street_id).all()
 
 def create_request(resident_id, drive_id):
-  request = Request(resident_id, drive_id)
-  db.session.add(resident)
-  db.session.commit()
-  return request
+    request = Request(resident_id, drive_id)
+    db.session.add(request)
+    db.session.commit()
+    return request
 
 def get_request_by_resident(resident_id):
-  return Request.query.filter_by(residentId = resident_id),all()
+    return Request.query.filter_by(residentId=resident_id).all()
