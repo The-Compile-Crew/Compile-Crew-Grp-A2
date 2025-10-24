8d74046ac63148254faa286625a56bb37af20e43
AishaLewis3 <aisha.lewis3@my.uwi.edu>
2025-10-23 20:21:33 -0400

    Fix Request model constructor to apply default status

--- App/models/request.py
+++ App/models/request.py
@@ -5,11 +5,12 @@ class Request(db.Model):
     residentId = db.Column(db.Integer, db.ForeignKey('resident.residentId'), nullable=False)
     driveId = db.Column(db.Integer, db.ForeignKey('drive.driveId'), nullable=False)
     status = db.Column(db.String(50), default="pending")
-    
-    def __init__(self, residentId, driveId):
+
+    def __init__(self, residentId, driveId, status="pending"):
         self.residentId = residentId
         self.driveId = driveId
-    
+        self.status = status
+
     def toJSON(self):
         return {
             'requestId': self.requestId,
