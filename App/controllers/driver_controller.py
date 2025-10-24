831a33bf2b850f8550c005b622533003ebd25434
AishaLewis3 <aisha.lewis3@my.uwi.edu>
2025-10-24 09:07:41 -0400

    Fix driver_controller naming and schedule_driver logic

--- App/controllers/driver_controller.py
+++ App/controllers/driver_controller.py
@@ -1,40 +1,40 @@
-from App.models import Driver
+from App.models import Driver, Drive, Request
 from App.database import db
 
 def create_driver(name):
-  driver = Driver(name)
-  db.session.add(driver)
-  db.session.commit()
-  return driver
+    driver = Driver(name)
+    db.session.add(driver)
+    db.session.commit()
+    return driver
 
 def get_all_drivers():
-  return Driver.query.all()
+    return Driver.query.all()
 
 def get_driver(driver_id):
-  return Driver.query.get(driver_id)
+    return Driver.query.get(driver_id)
 
 def update_driver_status(driver_id, status):
-  driver  = Driver.query.get(driver_id)
-  if driver:
-    driver.status = status
-    db.session.commit()
-  return driver
+    driver = Driver.query.get(driver_id)
+    if driver:
+        driver.status = status
+        db.session.commit()
+    return driver
 
 def update_driver_location(driver_id, location):
-  driver  = Driver.query.get(driver_id)
-  if driver:
-    driver.location = location
-    db.session.commit()
-  return driver
+    driver = Driver.query.get(driver_id)
+    if driver:
+        driver.location = location
+        db.session.commit()
+    return driver
 
-def schedule_driver(Driver_id, street_id, scheduled_time):
-  driver = Drive(driver_id, street_id, scheduled_time)
-  db.session.add(driver)
-  db.session.commit()
-  return driver
+def schedule_driver(driver_id, street_id, scheduled_time):
+    drive = Drive(driver_id, street_id, scheduled_time)
+    db.session.add(drive)
+    db.session.commit()
+    return drive
 
 def get_drives_by_driver(driver_id):
-  return Drive.query.filter_by(driverId = driver_id).all()
+    return Drive.query.filter_by(driverId=driver_id).all()
 
 def get_requests_by_driver(driver_id):
     return Request.query.join(Drive).filter(Drive.driverId == driver_id).all()
